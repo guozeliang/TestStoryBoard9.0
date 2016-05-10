1fecbca88d2756515089233901c49f25930bd4cf
guozeliang <932356614@qq.com>
2016-05-10 10:48:56 +0800

    iOS9.0多个storyBoard开发test

---
+++ TestStoryBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestStoryBoard
+//
+//  Created by SXTJ on 16/5/10.
+//  Copyright © 2016年 SXTJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestStoryBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestStoryBoard
+//
+//  Created by SXTJ on 16/5/10.
+//  Copyright © 2016年 SXTJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
